c89691874b7feab060c543a034ee248a85426918
jmart-9 <jersonmart9@gmail.com>
2025-04-25 17:11:59 -0600

    css
    css

--- js/scriptmenu.js
+++ js/scriptmenu.js
@@ -1,4 +1,13 @@
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');  // Alterna la clase "active" para mostrar/ocultar el menú
+
+    if (navLinks.classList.contains('active')) {
+        const links = navLinks.querySelectorAll('.nav-link');
+        links.forEach(link => {
+            link.addEventListener('click', () => {
+                navLinks.classList.remove('active'); // Oculta el menú al hacer clic en un enlace
+            }, { once: true }); // Solo se ejecuta una vez por enlace
+        });
+    }
 }
