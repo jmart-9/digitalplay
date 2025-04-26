c0721ecd3d82ad090363ef706b4d1c71adfd6834
jmart-9 <jersonmart9@gmail.com>
2025-04-26 08:10:28 -0600

    nav
    Navbar terminada

--- js/scriptmenu.js
+++ js/scriptmenu.js
@@ -2,8 +2,9 @@ function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');  // Alterna la clase "active" para mostrar/ocultar el menú
 
+    // Agregar un evento de clic a cada enlace solo si el menú está activo
     if (navLinks.classList.contains('active')) {
-        const links = navLinks.querySelectorAll('.nav-link');
+        const links = navLinks.querySelectorAll('li a'); // Selecciona todos los enlaces dentro de los elementos <li>
         links.forEach(link => {
             link.addEventListener('click', () => {
                 navLinks.classList.remove('active'); // Oculta el menú al hacer clic en un enlace
